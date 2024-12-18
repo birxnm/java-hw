6e17eabc442d1373a0da465d118d4a45c282b7e3
birxnm <240467@astanait.edu.kz>
2024-12-18 15:34:41 +0500

    Add files via upload

---
+++ Artexhibiton_management.java
@@ -0,0 +1,25 @@
+public class Artexhibiton_management {
+    private String Artwork;
+    private String Artist;
+    private String Gallery;
+
+    public String getArtwork() {
+        return Artwork;
+    }
+    public void setArtwork(String Artwork) {
+        this.Artwork = Artwork;
+    }
+    public String getArtist() {
+        return Artist;
+    }
+    public void setArtist(String Artist) {
+        this.Artist = Artist;
+    }
+    public String getGallery(){
+        return Gallery;
+    }
+    public void setGallery(String Gallery){
+        this.Gallery = Gallery;
+    }
+
+}
